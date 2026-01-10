9985709851caf2031e302fa88748a735384d4a5b
mareerray <mareerray@gmail.com>
2026-01-10 17:56:05 +0200

    Make UserServiceApplicationTests plain JUnit (no Spring context)

--- backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
@@ -1,15 +1,14 @@
 package com.buyone.userservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest(classes = {})
-@ContextConfiguration(classes = {})
+// @SpringBootTest(classes = {})
+// @ContextConfiguration(classes = {})
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test to check if the application context loads successfully
 	}
 
 }
