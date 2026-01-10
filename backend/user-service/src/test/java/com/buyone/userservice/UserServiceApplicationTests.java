8d21357ec54135460c50c8c66ff6d3d1fdbf2e94
mareerray <mareerray@gmail.com>
2026-01-10 17:27:14 +0200

    Simplify UserServiceApplicationTests to avoid Mongo/Auth beans

--- backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
@@ -2,15 +2,10 @@ package com.buyone.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {
-        MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class
-})
+@EnableAutoConfiguration(exclude = {})
 class UserServiceApplicationTests {
 
 	@Test
