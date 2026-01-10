1ad9e0352349bf6533d7ad8fd8465a0196d79ce7
mareerray <mareerray@gmail.com>
2026-01-10 17:22:47 +0200

    Disable Mongo auto-config in tests to avoid DB connection

--- backend/product-service/src/test/java/com/buyone/productservice/ProductServiceApplicationTests.java
+++ backend/product-service/src/test/java/com/buyone/productservice/ProductServiceApplicationTests.java
@@ -1,10 +1,17 @@
 package com.buyone.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class ProductServiceApplicationTests {
+@EnableAutoConfiguration(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
+class ProductServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
--- backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
@@ -1,9 +1,16 @@
 package com.buyone.userservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class
+})
 class UserServiceApplicationTests {
 
 	@Test
