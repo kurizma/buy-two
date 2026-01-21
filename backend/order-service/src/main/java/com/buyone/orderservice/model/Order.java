ce7b0581a15d6a6f799d95c2b12f773c3e2b92bf
kurizma <foriamkuri@gmail.com>
2026-01-21 17:44:13 +0200

    order-model edit

--- backend/order-service/src/main/java/com/buyone/orderservice/model/Order.java
+++ backend/order-service/src/main/java/com/buyone/orderservice/model/Order.java
@@ -1,12 +1,17 @@
 package com.buyone.orderservice.model;
 
 import lombok.*;
-import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.buyone.orderservice.model.OrderStatus.PENDING;
+import static com.buyone.orderservice.model.PaymentMethod.PAY_ON_DELIVERY;
 
 @Document(collection = "orders")
 @Data
@@ -17,22 +22,31 @@ public class Order {
     @Id
     private String id;
     
-    private String userId;       // Buyer
+    @Indexed
+    private String userId;       // "Buyer" - CLIENT Role Only
+    
+    @Indexed(unique = true)
     private String orderNumber;  // Unique: "ORD-" + UUID
     
     @Builder.Default
     private List<OrderItem> items = new ArrayList<>();
     
     @Builder.Default
-    private String status = "PENDING";
+    private OrderStatus status = PENDING;
     // "PENDING", "CONFIRMED", "SHIPPED", "DELIVERED", "CANCELLED"
     
-    private String paymentMethod = "PAY_ON_DELIVERY";
+    @Builder.Default
+    private PaymentMethod paymentMethod = PAY_ON_DELIVERY;
     
-    private RabbitConnectionDetails.Address shippingAddress;  // Inline or ref
-    private double subtotal;
-    private double tax;
-    private double total;
+    private Address shippingAddress;  // Inline or ref
+    
+    @Builder.Default
+    private BigDecimal subtotal;
+    @Builder.Default
+    private BigDecimal tax;
+    @Builder.Default
+    private BigDecimal total;
+    
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
