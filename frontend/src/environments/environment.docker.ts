5ff5eab9b6c135a011b1b689c115631857612d83
Toft <toft.dah@gmail.com>
2026-02-03 23:29:29 +0200

    Fix: connect front end to gateway

--- frontend/src/environments/environment.docker.ts
+++ frontend/src/environments/environment.docker.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:8080',
+  apiBaseUrl: 'https://157.180.64.182:8080',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:8080',
+  apiBaseUrl: 'https://157.180.64.182:8080',
 };
